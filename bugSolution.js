078c1f65b6d88f42d6811749e97b4409a203e18e
BugHunter-X <askzenai@gmail.com>
2025-01-17 10:38:41 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,9 @@
+function foo(a,b){
+if(typeof a === 'number' && typeof b === 'number'){
+return a+b; 
+} else {
+return "Error: Operands must be numbers";
+}
+}
+console.log(foo(1,2)); //3
+console.log(foo(1,"2")); //Error: Operands must be numbers
